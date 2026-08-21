2b112247102ac7c715f91dcc77d52af9f7850ce3
Meklasmaj <128758763+Meklasmaj@users.noreply.github.com>
2026-08-21 09:26:15 +0200

    Creating Interface IGenericRepo

---
+++ TheMovies.Core/Interfaces/IGenericRepo.cs
@@ -0,0 +1,10 @@
+﻿namespace TheMovies.Core.Interfaces;
+
+public interface IGenericRepo<T>
+{
+    T? Get(int id);
+    IEnumerable<T> GetAll();
+    void Add(T item);
+    void Update(T item);
+    void Remove(int id);
+}
