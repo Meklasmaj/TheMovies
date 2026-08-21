5b0bb43f66ed6ac3c18d5ca95a5a7988030d8b30
kmchng <kmchng@outlook.com>
2026-08-21 10:59:40 +0200

    Movie and Enum

---
+++ TheMovies.Core/Genre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheMovies.Core
+{
+    public enum genre
+    {
+        ActionandAdventure,
+        Comedy,
+        Crime,
+        Documentary,
+        TrueCrime,
+        Drama,
+        RomCom
+    }
+}
---
+++ TheMovies.Core/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TheMovies.Core;
+
+public class Movie
+{
+    //Properties
+    public string Title { get; set; }
+    public genre Genre { get; set; }    //enum type for genre
+    public int Duration { get; set; }
+
+    //Constructor
+    public Movie(string title, genre genre, int duration)
+    {
+        Title = title;
+        Genre = genre;
+        Duration = duration;
+    }
+
+}
