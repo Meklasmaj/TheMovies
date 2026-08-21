6bd66b41599fa90531ce574dd6b662b13999f7ff
Meklasmaj <128758763+Meklasmaj@users.noreply.github.com>
2026-08-21 09:37:22 +0200

    Creating FileRepository

---
+++ TheMovies.Core/Repositories/FileRepository.cs
@@ -0,0 +1,115 @@
+﻿using TheMovies.Core.Interfaces;
+using System.Text.Json;
+
+namespace TheMovies.Core.Repositories;
+
+public class FileRepository<T> : IGenericRepo<T> where T : IHasId
+{
+    private string _filePath;
+    private int _id = 0;
+
+    public FileRepository(string path)
+    {
+        _filePath = path;
+    }
+
+    public T? Get(int id)
+    {
+        return GetAll().FirstOrDefault(p => p.Id == id);
+    }
+
+    public IEnumerable<T> GetAll()
+    {
+        List<T> items = new List<T>();
+        string json = "";
+        try
+        {
+            //Read file and add products to list
+            if (File.Exists(_filePath))
+            {
+                json = File.ReadAllText(_filePath);
+            }
+            else
+            {
+                File.Create(_filePath).Close();
+            }
+
+            items = JsonSerializer.Deserialize<List<T>>(json);
+        }
+        catch (FileNotFoundException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (DirectoryNotFoundException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (JsonException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        return items;
+    }
+
+    public void Add(T item)
+    {
+        List<T> items = GetAll().ToList();
+        _id = items.Max(p => p.Id) + 1;
+        item.Id = _id;
+        items.Add(item);
+        Save(items);
+    }
+
+    public void Update(T item)
+    {
+        List<T> items = GetAll().ToList();
+        items[(items.IndexOf(items.Find(p => p.Id == item.Id)))] = item;
+        Save(items);
+    }
+
+    public void Remove(int id)
+    {
+        List<T> items = GetAll().ToList();
+        items.Remove(items.Find(p => p.Id == id));
+        Save(items);
+    }
+
+    public void Save(List<T> items)
+    {
+        //Removes .json and adds .tmp
+        string tempFilepath = $"{_filePath[..^5]}.tmp";
+
+        try
+        {
+            string json = JsonSerializer.Serialize(items, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(tempFilepath, json);
+            //Atomic Write Pattern
+            //Only renames the .tmp file to .json if the saving succeeded
+            File.Move(tempFilepath, _filePath, overwrite: true);
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine(e.Message);
+            File.Delete(tempFilepath);
+        }
+        catch (JsonException e)
+        {
+            Console.WriteLine(e.Message);
+            File.Delete(tempFilepath);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            File.Delete(tempFilepath);
+        }
+    }
+}
