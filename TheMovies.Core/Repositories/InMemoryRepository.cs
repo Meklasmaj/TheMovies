5909f68b6cca409af6fa15e6cc859d960d7350d4
Meklasmaj <128758763+Meklasmaj@users.noreply.github.com>
2026-08-21 09:33:16 +0200

    Creating InMemoryRepo

---
+++ TheMovies.Core/Repositories/InMemoryRepository.cs
@@ -0,0 +1,36 @@
+﻿using TheMovies.Core.Interfaces;
+
+namespace TheMovies.Core.Repositories;
+
+public class InMemoryRepository<T> : IGenericRepo<T> where T : IHasId
+{
+    private List<T> _items = new List<T>();
+    private int _id = 0;
+
+    public T? Get(int id)
+    {
+        return _items.Find(p => p.Id == id);
+    }
+
+    public IEnumerable<T> GetAll()
+    {
+        return new List<T>(_items);
+    }
+
+    public void Add(T item)
+    {
+        _id = _items.Max(p => p.Id) + 1;
+        item.Id = _id;
+        _items.Add(item);
+    }
+
+    public void Update(T item)
+    {
+        _items[(_items.IndexOf(_items.Find(p => p.Id == item.Id)))] = item;
+    }
+
+    public void Remove(int id)
+    {
+        _items.Remove(_items.Find(p => p.Id == id));
+    }
+}
