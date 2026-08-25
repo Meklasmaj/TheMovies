436a6e7767523856d639bf96a10aef70c07d2692
Meklasmaj <meklas@majlandt.dk>
2026-08-25 20:41:40 +0200

    Moving files to folders. Creating NavigationService

---
+++ TheMovies.UI/NavigationService.cs
@@ -0,0 +1,16 @@
+﻿namespace TheMovies.UI;
+
+public class NavigationService
+{
+    private readonly MainViewModel _mainViewModel;
+
+    public NavigationService(MainViewModel mainViewModel)
+    {
+        _mainViewModel = mainViewModel;
+    }
+
+    /*public void NavigateTo<T>() where T : ViewModelBase, new()
+    {
+        _mainViewModel.CurrentViewModel = new T();
+    }*/
+}
