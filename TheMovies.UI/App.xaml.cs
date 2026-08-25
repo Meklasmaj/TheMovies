fd7457a9e2d26d723b086ef7c83545ba8e8b6fc7
Meklasmaj <meklas@majlandt.dk>
2026-08-25 20:57:36 +0200

    Updating App to support switching views and adding comments

--- TheMovies.UI/App.xaml.cs
+++ TheMovies.UI/App.xaml.cs
@@ -9,4 +9,23 @@ namespace TheMovies.UI;
 /// </summary>
 public partial class App : Application
 {
+    //static ViewModelBase viewModelBase = new ViewModelBase();
+    //public static NavigationService NavigationService = new NavigationService(viewModelBase);
+    
+    // Overrides method that runs on startup
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        // Does the normal startup
+        base.OnStartup(e);
+        
+        // Creates new Window
+        var mainWindow = new MainWindow()
+        {
+            // Setting DataContext of the new Window to viewModelBase
+            //DataContext = viewModelBase
+        };
+        
+        // Opens new window
+        mainWindow.Show();
+    }
 }
