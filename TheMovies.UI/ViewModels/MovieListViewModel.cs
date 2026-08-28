3c320322f3df6a427a3cda42279791caeb52c572
Meklasmaj <128758763+Meklasmaj@users.noreply.github.com>
2026-08-28 23:13:23 +0200

    Creating MovieListView and MovieListViewModel

---
+++ TheMovies.UI/ViewModels/MovieListViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+using TheMovies.Core;
+
+namespace TheMovies.UI.ViewModels;
+
+public class MovieListViewModel : ViewModelBase
+{
+    public ObservableCollection<Movie> Movies { get; set; }
+    public RelayCommand GoBackCommand { get; set; }
+
+    public MovieListViewModel()
+    {
+        Movies = new ObservableCollection<Movie>(movieRepository.GetAll());
+        GoBackCommand = new RelayCommand(GoBack);
+    }
+
+    public void GoBack(object parameter)
+    {
+        // Navigates back to main
+        App.NavigationService.NavigateTo<MainViewViewModel>();
+    }
+}
