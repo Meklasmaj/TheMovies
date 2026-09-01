e812ad46d18144527b1437d664f50826289e0ca9
Meklasmaj <meklas@majlandt.dk>
2026-09-01 15:55:40 +0200

    Creating TestProject

---
+++ TheMovies.Tests/Test1.cs
@@ -0,0 +1,18 @@
+﻿using TheMovies.Core.Interfaces;
+using TheMovies.Core;
+using TheMovies.Core.Repositories;
+
+namespace TheMovies.Tests;
+
+[TestClass]
+public sealed class Test1
+{
+    // RAM Repo for testing
+    public IGenericRepo<Movie> MovieRepo { get; set; } = new InMemoryRepository<Movie>();
+    
+    [TestMethod]
+    public void RepoAddMovieSuccessfullyAddedToList()
+    {
+        
+    }
+}
