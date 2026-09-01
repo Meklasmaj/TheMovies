0a47ff3f74a7940e3acd873c8da6fbd483e8e6b5
Meklasmaj <meklas@majlandt.dk>
2026-09-01 16:11:44 +0200

    Writing 6 working unittests for Movie CRUD

--- TheMovies.Tests/Test1.cs
+++ TheMovies.Tests/Test1.cs
@@ -7,12 +7,94 @@ namespace TheMovies.Tests;
 [TestClass]
 public sealed class Test1
 {
-    // RAM Repo for testing
-    public IGenericRepo<Movie> MovieRepo { get; set; } = new InMemoryRepository<Movie>();
     
     [TestMethod]
     public void RepoAddMovieSuccessfullyAddedToList()
     {
+        // RAM Repo for testing
+        IGenericRepo<Movie> movieRepo = new InMemoryRepository<Movie>();
         
+        movieRepo.Add(new Movie("Spiderman", MovieGenre.ActionandAdventure, 120, "Tim Burton", DateTime.Now));
+        movieRepo.Add(new Movie("BatMan", MovieGenre.Comedy, 130, "Tim Burton", DateTime.Now));
+        movieRepo.Add(new Movie("IronMan", MovieGenre.Drama, 160, "Tim Burton", DateTime.Now));
+        
+        Assert.AreEqual(3, movieRepo.GetAll().Count());
+    }
+    
+    [TestMethod]
+    public void RepoAddMovieSuccessfullyAddedIdToMovie()
+    {
+        // RAM Repo for testing
+        IGenericRepo<Movie> movieRepo = new InMemoryRepository<Movie>();
+        
+        movieRepo.Add(new Movie("Spiderman", MovieGenre.ActionandAdventure, 120, "Tim Burton", DateTime.Now));
+        movieRepo.Add(new Movie("BatMan", MovieGenre.Comedy, 130, "Tim Burton", DateTime.Now));
+        movieRepo.Add(new Movie("IronMan", MovieGenre.Drama, 160, "Tim Burton", DateTime.Now));
+        
+        Assert.AreEqual(2, movieRepo.GetAll().ElementAt(2).Id);
+        Assert.AreEqual(0, movieRepo.GetAll().ElementAt(0).Id);
+    }
+    
+    [TestMethod]
+    public void RepoAddMovieSuccessfullyAddedMovieToCorrectPlace()
+    {
+        // RAM Repo for testing
+        IGenericRepo<Movie> movieRepo = new InMemoryRepository<Movie>();
+        
+        movieRepo.Add(new Movie("Spiderman", MovieGenre.ActionandAdventure, 120, "Tim Burton", DateTime.Now));
+        movieRepo.Add(new Movie("BatMan", MovieGenre.Comedy, 130, "Tim Burton", DateTime.Now));
+        movieRepo.Add(new Movie("IronMan", MovieGenre.Drama, 160, "Tim Burton", DateTime.Now));
+        
+        Assert.AreEqual("BatMan", movieRepo.GetAll().ElementAt(1).title);
+    }
+    
+    [TestMethod]
+    public void RepoRemoveMovieSuccessfullyRemovedMovie()
+    {
+        // RAM Repo for testing
+        IGenericRepo<Movie> movieRepo = new InMemoryRepository<Movie>();
+        
+        movieRepo.Add(new Movie("Spiderman", MovieGenre.ActionandAdventure, 120, "Tim Burton", DateTime.Now));
+        movieRepo.Add(new Movie("BatMan", MovieGenre.Comedy, 130, "Tim Burton", DateTime.Now));
+        movieRepo.Add(new Movie("IronMan", MovieGenre.Drama, 160, "Tim Burton", DateTime.Now));
+        
+        movieRepo.Remove(1);
+        
+        Assert.IsNull(movieRepo.GetAll().FirstOrDefault(x => x.Id == 1));
+        Assert.IsNull(movieRepo.GetAll().FirstOrDefault(x => x.title == "BatMan"));
+        Assert.IsNotNull(movieRepo.GetAll().FirstOrDefault(x => x.Id == 2));
+    }
+    
+    [TestMethod]
+    public void RepoUpdateMovieSuccessfullyUpdatedMovie()
+    {
+        // RAM Repo for testing
+        IGenericRepo<Movie> movieRepo = new InMemoryRepository<Movie>();
+        
+        movieRepo.Add(new Movie("Spiderman", MovieGenre.ActionandAdventure, 120, "Tim Burton", DateTime.Now));
+        movieRepo.Add(new Movie("BatMan", MovieGenre.Comedy, 130, "Tim Burton", DateTime.Now));
+        movieRepo.Add(new Movie("IronMan", MovieGenre.Drama, 160, "Tim Burton", DateTime.Now));
+
+        Movie movie = movieRepo.Get(1);
+        movie.title = "Superman";
+        movieRepo.Update(movie);
+        
+        Assert.IsNull(movieRepo.GetAll().FirstOrDefault(x => x.title == "BatMan"));
+        Assert.AreEqual("Superman", movieRepo.GetAll().ElementAt(1).title);
+    }
+    
+    [TestMethod]
+    public void RepoGetMovieByIdSuccessfullyRetrievedMovie()
+    {
+        // RAM Repo for testing
+        IGenericRepo<Movie> movieRepo = new InMemoryRepository<Movie>();
+        
+        movieRepo.Add(new Movie("Spiderman", MovieGenre.ActionandAdventure, 120, "Tim Burton", DateTime.Now));
+        movieRepo.Add(new Movie("BatMan", MovieGenre.Comedy, 130, "Tim Burton", DateTime.Now));
+        movieRepo.Add(new Movie("IronMan", MovieGenre.Drama, 160, "Tim Burton", DateTime.Now));
+
+        Movie movie = movieRepo.Get(1);
+        
+        Assert.AreEqual(movie, movieRepo.GetAll().ElementAt(1));
     }
 }
