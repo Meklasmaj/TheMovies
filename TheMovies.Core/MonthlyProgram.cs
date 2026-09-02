b60f7e10e09d140f2c4d8ae4a5dcc653722f490f
kmchng <kmchng@outlook.com>
2026-09-02 14:41:48 +0200

    Add core cinema management entities to TheMovies.Core
    Added Cinema, MonthlyProgram, Screen, and Show classes to TheMovies.Core namespace. These classes model core entities for cinema management, including properties, constructors, and methods for managing relationships. Show class includes a custom ToString override for formatted output.

---
+++ TheMovies.Core/MonthlyProgram.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheMovies.Core.Interfaces;
+
+namespace TheMovies.Core;
+
+public class MonthlyProgram : IHasId
+{
+    public int Id { get; set; }
+    public int Month { get; set; }
+    public int Year { get; set; } = 0;
+    public Cinema Cinema { get; set; }
+
+    public List<Show> Shows { get; set; } = new List<Show>();
+
+    public MonthlyProgram(int month, int year, Cinema cinema)
+    {
+        this.Month = month;
+        this.Year = year;
+        this.Cinema = cinema;
+    }
+
+    public void AddShow(Show show)
+    {
+        Shows.Add(show);
+    }
+
+    public void RemoveShow(Show show) 
+    { 
+        Shows.Remove(show); 
+    }
+
+}
+
---
+++ TheMovies.Core/Show.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheMovies.Core.Interfaces;
+
+namespace TheMovies.Core;
+
+    public class Show : IHasId
+    {
+        public int Id { get; set; }
+        public Movie Movie { get; set; }
+        public Screen Screen { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime startTime { get; set; }
+        public int playTime { get; set; }
+
+    //Constructor
+    public Show(Movie movie, Screen screen, DateTime date, DateTime startTime, int playTime)
+    {
+        Movie = movie;
+        Screen = screen;
+        Date = date;
+        startTime = startTime;
+        playTime = playTime;
+    }
+
+    public override string ToString()
+        {
+            return $"{Movie.title,-25}{Screen.screenNumber,-15}{Date.ToShortDateString(),-15}{startTime.ToShortTimeString(),-15}{playTime,-15}";
+        }
+  
+}
